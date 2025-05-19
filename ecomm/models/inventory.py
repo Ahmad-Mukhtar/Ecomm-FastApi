edbf14dd4580a7a2f5ca9682e5497e90e0910994
Ahmad Mukhtar <gracenoteii5@gmail.com>
2025-05-19 21:08:06 +0500

    Revert "init"
    This reverts commit 39cec9722e66ea715ceb84e2a7dcbe9722e702b9.

--- ecomm/models/inventory.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class InventoryUpdate(BaseModel):
-    product_id: int
-    quantity: int
--- ecomm/models/product.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-
-class Product(BaseModel):
-    id: Optional[int]
-    name: str
-    category: str
-    price: float
